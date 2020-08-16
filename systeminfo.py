8ee6bb0392e2930de49129b28e7876566b7034fe
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2020-08-16 02:45:37 +0200

    Marked dummy values with an asterisk at EOL

--- systeminfo.py
+++ systeminfo.py
@@ -112,76 +112,76 @@ def main():
     systeminfo.update(parse_software_hive(software_hive))
     output = f"""Host Name:                 {systeminfo['hostname']}
 OS Name:                   {systeminfo['os_name']}
-OS Version:                10.0.17134 N/A Build 17134
-OS Manufacturer:           Microsoft Corporation
-OS Configuration:          Standalone Workstation
+OS Version:                10.0.17134 N/A Build 17134 *
+OS Manufacturer:           Microsoft Corporation *
+OS Configuration:          Standalone Workstation *
 OS Build Type:             {systeminfo['os_build_type']}
 Registered Owner:          {systeminfo['registered_owner']}
-Registered Organization:
+Registered Organization:   *
 Product ID:                {systeminfo['product_id']}
 Original Install Date:     {systeminfo['install_date']}  # TODO Add timezone offset and convert string
 System Boot Time:          0-0-0000, 00:00:00
 System Manufacturer:       {systeminfo['manufacturer']}
 System Model:              {systeminfo['model']}
 System Type:               {systeminfo['type']}
-Processor(s):              1 Processor(s) Installed.
-                           [01]: Intel64 Family 6 Model 78 Stepping 3 GenuineIntel ~2396 Mhz
+Processor(s):              1 Processor(s) Installed. *
+                           [01]: Intel64 Family 6 Model 78 Stepping 3 GenuineIntel ~2396 Mhz *
 BIOS Version:              {systeminfo['bios_version']}
-Windows Directory:         C:\WINDOWS
-System Directory:          C:\WINDOWS\system32
+Windows Directory:         C:\WINDOWS *
+System Directory:          C:\WINDOWS\system32 *
 Boot Device:               {systeminfo['boot_device']}
-System Locale:             en-us;English (United States)
-Input Locale:              en-us;English (United States)
-Time Zone:                 (UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna
-Total Physical Memory:     16.265 MB
-Available Physical Memory: 5.160 MB
-Virtual Memory: Max Size:  18.697 MB
-Virtual Memory: Available: 4.793 MB
-Virtual Memory: In Use:    13.904 MB
+System Locale:             en-us;English (United States) *
+Input Locale:              en-us;English (United States) *
+Time Zone:                 (UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna *
+Total Physical Memory:     16.265 MB *
+Available Physical Memory: 5.160 MB *
+Virtual Memory: Max Size:  18.697 MB *
+Virtual Memory: Available: 4.793 MB *
+Virtual Memory: In Use:    13.904 MB *
 Page File Location(s):     """
     padding = ''
     for page_file_location in systeminfo['page_file_locations']:
         output += f'{padding}{page_file_location}\n'
         padding = '                           '
     output += f"""Domain:                    {systeminfo['domain']}
-Logon Server:              \\LAPTOP
+Logon Server:              \\LAPTOP *
 Hotfix(s):                 {len(systeminfo['hotfix'])} Hotfix(s) Installed.
 """
     for idx, hotfix in enumerate(systeminfo['hotfix'], start=1):
         output += f'                           [{str(idx).zfill(2)}]: {hotfix}\n'
-    output += """Network Card(s):           6 NIC(s) Installed.
-                           [01]: TAP-Windows Adapter V9
-                                 Connection Name: Ethernet 2
-                                 Status:          Media disconnected
-                           [02]: VMware Virtual Ethernet Adapter for VMnet1
-                                 Connection Name: VMware Network Adapter VMnet1
-                                 DHCP Enabled:    No
-                                 IP address(es)
-                                 [01]: 192.168.150.1
-                                 [02]: fe80::8900:6f73:9629:d8f3
-                           [03]: VMware Virtual Ethernet Adapter for VMnet8
-                                 Connection Name: VMware Network Adapter VMnet8
-                                 DHCP Enabled:    No
-                                 IP address(es)
-                                 [01]: 192.168.159.1
-                                 [02]: fe80::984a:e9de:308b:2457
-                           [04]: Intel(R) Ethernet Connection I219-LM
-                                 Connection Name: Ethernet
-                                 Status:          Media disconnected
-                           [05]: Intel(R) Dual Band Wireless-AC 8260
-                                 Connection Name: Wi-Fi
-                                 DHCP Enabled:    Yes
-                                 DHCP Server:     192.168.1.1
-                                 IP address(es)
-                                 [01]: 192.168.1.57
-                                 [02]: fe80::391d:20cc:b273:851c
-                           [06]: Generic Mobile Broadband Adapter
-                                 Connection Name: Cellular
-                                 Status:          Media disconnected
-Hyper-V Requirements:      VM Monitor Mode Extensions: Yes
-                           Virtualization Enabled In Firmware: Yes
-                           Second Level Address Translation: Yes
-                           Data Execution Prevention Available: Yes
+    output += """Network Card(s):           6 NIC(s) Installed. *
+                           [01]: TAP-Windows Adapter V9 *
+                                 Connection Name: Ethernet 2 *
+                                 Status:          Media disconnected *
+                           [02]: VMware Virtual Ethernet Adapter for VMnet1 *
+                                 Connection Name: VMware Network Adapter VMnet1 *
+                                 DHCP Enabled:    No *
+                                 IP address(es) *
+                                 [01]: 192.168.150.1 *
+                                 [02]: fe80::8900:6f73:9629:d8f3 *
+                           [03]: VMware Virtual Ethernet Adapter for VMnet8 *
+                                 Connection Name: VMware Network Adapter VMnet8 *
+                                 DHCP Enabled:    No *
+                                 IP address(es) *
+                                 [01]: 192.168.159.1 *
+                                 [02]: fe80::984a:e9de:308b:2457 *
+                           [04]: Intel(R) Ethernet Connection I219-LM *
+                                 Connection Name: Ethernet *
+                                 Status:          Media disconnected *
+                           [05]: Intel(R) Dual Band Wireless-AC 8260 *
+                                 Connection Name: Wi-Fi *
+                                 DHCP Enabled:    Yes *
+                                 DHCP Server:     192.168.1.1 *
+                                 IP address(es) *
+                                 [01]: 192.168.1.57 *
+                                 [02]: fe80::391d:20cc:b273:851c *
+                           [06]: Generic Mobile Broadband Adapter *
+                                 Connection Name: Cellular *
+                                 Status:          Media disconnected *
+Hyper-V Requirements:      VM Monitor Mode Extensions: Yes *
+                           Virtualization Enabled In Firmware: Yes *
+                           Second Level Address Translation: Yes *
+                           Data Execution Prevention Available: Yes *
 """
     print(output)
 
